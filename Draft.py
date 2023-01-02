f2d30ff47e17e2ace7f1233c79356c7519b27be7
Quinn Le <quynhnle135@gmail.com>
2023-01-01 21:33:25 -0500

    Learned how to create and call functions in Python

--- Draft.py
+++ Draft.py
@@ -1,1 +1,24 @@
-print('hello quinn')
+def say_my_name(name):
+    print('Hi my name is', name)
+
+def basic_sum(a, b):
+    return a + b
+
+
+def print_values(array):
+    for i in array:
+        print(i, end=" ")
+
+
+def print_numbers():
+    for i in range(2, 11, 2):
+        print(i, end=" ")
+
+
+say_my_name('Hoshi')
+basic_sum(1, 2)
+print(basic_sum(1, 2))
+my_arr = [1, 2, 3, 4]
+print_values(my_arr)
+print()
+print_numbers()
